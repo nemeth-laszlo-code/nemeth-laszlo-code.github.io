d0ca1df9ff881c93fa7afbb88d08fc306b47ba52
Laszlo Nemeth <nemeth.rzr@gmail.com>
2026-04-24 17:33:58 +0200

    Updates

--- _nuxt/BLA1mmbP.js
+++
@@ -1,1 +0,0 @@
-import{aj as a,a as s,ak as r,K as u,al as o}from"./D4fx-wol.js";function d(t){const e=t||s();return e.ssrContext?.head||e.runWithContext(()=>{if(r()){const n=u(o);if(!n)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return n}})}function i(t,e={}){const n=e.head||d(e.nuxt);return a(t,{head:n,...e})}export{i as u};
